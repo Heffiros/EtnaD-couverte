eb187afddc96268fd24fe7ebf64c279a6b8a5d7d
Alexandre Levy <levy_a@etna-alternance.net>
2018-05-25 15:30:55 +0200

    first

---
+++ Assets/Move.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speedMoove = 7.0f;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("yoloo");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speedMoove * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(Vector3.left * speedMoove * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.Translate(Vector3.forward * speedMoove * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speedMoove * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -1, 0);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+    }
+}
